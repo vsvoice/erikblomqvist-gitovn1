b5214d7082644e0adae4532560a83ae9c5f2f201
vsvoice <erikblomqvist1@gmail.com>
2018-08-29 13:56:39 +0300

    script.js gjord och länkad till index.html

--- lotto/js/script.js
+++ lotto/js/script.js
@@ -1,10 +1,10 @@
 //slumptal mellan 1 och 39
 //OBS dålig kod, mjlighet till dublikat => använd inte ;)
 function lottoRad() {
+	alert("lottoraden klar");
 	for (var i=0; i<=7; i++) {
 
 		let lottery = Math.floor(Math.random() * 38)+1;
 	 	document.getElementById ("lotto_" + i).innerHTML = lottery;
-
 	}
 }
